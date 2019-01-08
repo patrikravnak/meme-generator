e88caeba78742c3cde4e093a51920673ed389408
patrikravnak <patrik.ravnak@gmail.com>
2019-01-08 12:06:49 +0100

    Dodan Main

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Meme from './Meme'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="container main-content">
+                <div className="grid">
+                    <Meme />
+                    <Meme />
+                    <Meme />
+                    <Meme />
+                    <Meme />
+                    <Meme />
+                </div>
+            </div>
+        );
+    }
+}
