5d92c114f6a204178859107e8a8d200fa26776d3
patrikravnak <patrik.ravnak@gmail.com>
2019-01-08 12:06:35 +0100

    Dodani komponenti Header in Main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+import Header from './components/Header';
+import Main from './components/Main';
+
 class App extends Component {
   render() {
-    return <div>Pozdravljen svet!</div>;
+    return (
+      <React.Fragment>
+        <Header />
+        <Main />
+      </React.Fragment>
+    );
   }
 }
 
